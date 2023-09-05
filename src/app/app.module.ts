751dd92ee6354bf05f42b5e6d4b0283e2e544d1f
benfran21 <benfran21@hotmail.com>
2023-09-05 12:40:59 -0400

    Avance del sitio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
+import { logginComponent } from './loggin/loggin.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { BodyComponent } from './body/body.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { EpisodiosComponent } from './episodios/episodios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    logginComponent,
+    PersonajesComponent,
+    BodyComponent,
+    EpisodiosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatPaginatorModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
